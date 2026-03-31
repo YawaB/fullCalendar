af616bff25bca33f31b3f2c2fd6108ef9a00f04f
yawab <mawuviwobrinda@gmail.com>
2026-03-31 16:55:33 +0100

    add npm config

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,31 +1,36 @@
 import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/easycal.cjs.js',
-      format: 'cjs',
-      exports: 'default',
-      sourcemap: true
-    },
-    {
-      file: 'dist/easycal.esm.js',
-      format: 'esm',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    postcss({
-      extract: 'easycal.css',
-      minimize: true,
-      sourceMap: true
-    }),
-    typescript({
-      tsconfig: './tsconfig.json',
-      useTsconfigDeclarationDir: true,
-      clean: true
-    })
-  ]
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'dist/easycal.cjs.js',
+            format: 'cjs',
+            exports: 'default',
+            sourcemap: true
+        },
+        {
+            file: 'dist/easycal.esm.js',
+            format: 'esm',
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        resolve({
+            extensions: ['.mjs', '.js', '.json', '.ts']
+        }),        commonjs(),
+        typescript({
+            tsconfig: './tsconfig.json',
+            useTsconfigDeclarationDir: true,
+            clean: true
+        }),
+        postcss({
+            extract: 'easycal.css',
+            minimize: true,
+            sourceMap: true
+        }),
+    ]
 };
